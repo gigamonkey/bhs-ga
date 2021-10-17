65db6ed3422d3cf5929bd51fd8c9c541fc173376
Peter Seibel <peter@gigamonkeys.com>
2021-10-17 07:17:58 +0000

    Dumb code golf.

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const letters = "abcdefghijklmnopqrstuvwxyz";
 const digits = "0123456789";
 const alphabet = letters + letters.toUpperCase() + digits + " .,;:!?";
-
 const phrase = "To be, or not to be, that is the question.";
 
 function run(target, populationSize, maxGenerations, mutationRate = 0.01) {
   let population = randomPopulation(populationSize, target.length);
-  for (let i = 0; i < maxGenerations; i++) {
+  let currentBest = null;
+  for (let i = 0; i < maxGenerations && currentBest !== target; i++) {
     let scores = allScores(target, population);
     let parents = selectParents(population, scores);
-    let currentBest = parents[0];
+    currentBest = parents[0];
     logCurrentState(i, scores, population, currentBest);
-    if (currentBest === target) {
-      break;
-    }
     population = makeBabies(parents, mutationRate);
   }
 }
@@ -37,7 +34,7 @@ function selectParents(population, scores) {
 }
 
 function makeBabies(parents, mutationRate) {
-  let matches = shuffleArray(Array.from(parents));
+  let matches = shuffle(Array.from(parents));
   let babies = [];
   for (let i = 0; i < matches.length - 1; i += 2) {
     for (let j = 0; j < 2; j++) {
@@ -57,27 +54,22 @@ function cross(p1, p2) {
   ];
 }
 
-function mutate(critter, mutationRate) {
-  function maybeRandom(c) {
-    return Math.random() < mutationRate ? randomCharacter() : c;
-  }
-  return Array.from(critter).map(maybeRandom).join("");
+function mutate(critter, rate) {
+  return Array.from(critter).map(c => Math.random() < rate ? randomCharacter() : c).join("");
 }
 
 function allScores(target, population) {
   let scores = {};
   for (let critter of population) {
     if (!(critter in scores)) {
-      scores[critter] = scoreCritter(target, critter);
+      scores[critter] = fitness(target, critter);
     }
   }
   return scores;
 }
 
-function scoreCritter(target, critter) {
-  return Array.from(critter)
-    .map((c, i) => (target[i] === c ? 1 : 0))
-    .reduce((a, b) => a + b);
+function fitness(target, critter) {
+  return Array.from(critter).map((c, i) => (target[i] === c)).reduce((a, b) => a + b);
 }
 
 function logCurrentState(i, scores, population, mostFit) {
@@ -85,13 +77,13 @@ function logCurrentState(i, scores, population, mostFit) {
   let best = Object.values(scores).reduce((a, b) => Math.max(a, b), -Infinity);
   let worst = Object.values(scores).reduce((a, b) => Math.min(a, b), +Infinity);
   let avg = Array.from(population).reduce((acc, c) => acc + scores[c], 0) / population.length;
-  
+
   console.log(`Generation ${i}: ${num} unique critters out of ${population.length}`);
   console.log(`Average fitness: ${avg}. Most fit: ${best}. Least fit: ${worst}`);
   console.log(`Current best: ${mostFit}\n`);
 }
 
-function shuffleArray(array) {
+function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
